0855f76ecaa5eb3dbef96f57e3162e2d608ffb51
HectorPR4546 <h_perez93@yahoo.com>
2025-08-22 11:37:04 -0400

    Fix: Add setters back and keep validation in constructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -28,4 +28,25 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
 }
