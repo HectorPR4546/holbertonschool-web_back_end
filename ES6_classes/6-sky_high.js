d6da060d8c76777d9f5598de7b36d0a8354986be
HectorPR4546 <h_perez93@yahoo.com>
2025-08-23 11:28:19 -0400

    Task 6: Implement a class named SkyHighBuilding

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+import Building from './5-building.js';
+
+/**
+ * Represents a sky-high building.
+ */
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
