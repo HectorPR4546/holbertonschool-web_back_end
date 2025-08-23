f86e4f7e96c214dff4e3df8682ce2da4ddf20a0c
HectorPR4546 <h_perez93@yahoo.com>
2025-08-23 11:37:45 -0400

    Task 10: Implement a class named Car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+/**
+ * Represents a car.
+ */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
