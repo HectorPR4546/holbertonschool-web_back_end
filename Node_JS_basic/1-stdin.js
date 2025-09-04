7944a53ec88ec861553213d4b0979e5cecbf7ca7
HectorPR4546 <h_perez93@yahoo.com>
2025-09-04 10:02:00 -0400

    Node_JS_basic: replace 1-stdin.js with working readable/close implementation (Task 1)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,12 @@
-/*
- * Program that reads a name from stdin and prints it.
- */
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-let responded = false;
-
-process.stdin.on('data', (data) => {
-  if (responded) return;
-  const str = data.toString();
-  const input = str.split(/\r?\n/)[0];
-  console.log(`Your name is: ${input}`);
-  responded = true;
-  if (process.stdin.isTTY) {
-    process.exit(0);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
